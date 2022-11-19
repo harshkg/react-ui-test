423ea8b54f0fed74d345cccb4a77ebe2b9556f1f
harshkg <harsh140198@gmail.com>
2022-11-19 20:43:03 +0530

    class and props

--- src/App.js
+++ src/App.js
@@ -5,6 +5,9 @@ import Greet1 from './componenet/Greet1';
 import Welcome from './componenet/Welcome';
 import Hello from './componenet/Hello';
 import Hello1 from './componenet/Hello1';
+import GreetProps from './componenet/Greet-props';
+import WelcomeProps from './componenet/Welcome-props';
+import Message from './componenet/Message';
 
 function App() {
   return (
@@ -19,6 +22,15 @@ function App() {
         <Welcome/>
         <Hello/>
         <Hello1/>
+        <GreetProps name='Bruce' heroName='Batman'>
+          <p>Hello Batsy</p>
+        </GreetProps>
+        <GreetProps name='Clark' heroName='Superman'/>
+        <GreetProps name='Diana' heroName='Wonder Woman'/>
+        <WelcomeProps name='Bruce' heroName='Batman' />
+        <WelcomeProps name='Clark' heroName='Superman'/>
+        <WelcomeProps name='Diana' heroName='Wonder Woman'/>
+        <Message />
         <a
           className="App-link"
           href="https://reactjs.org"
